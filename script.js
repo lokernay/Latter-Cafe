68cb05eac7cb8cee83fec257c3b5acdc19e3919b
unknown <grifernter@mail.ru>
2023-05-07 20:09:22 +0400

    Site Latte cafe

---
+++ script.js
@@ -0,0 +1,17 @@
+function changeImgae1()
+{
+    var img = document.getElementById("image")
+    img.src = "img/IMAGE.png"
+}
+
+function changeImgae2()
+{
+    var img = document.getElementById("image")
+    img.src = "img/Drinks.png"
+}
+
+function changeImgae3()
+{
+    var img = document.getElementById("image")
+    img.src = "img/Takeaway.jpg"
+}
